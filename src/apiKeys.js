f39dec6311c845cb461a2ad941a95f737644511f
mayanktiwari4321 <mayank570.be22@chitkara.edu.in>
2024-10-03 10:18:51 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b98a4f4d016b5dbd946eb2f875971a16",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
